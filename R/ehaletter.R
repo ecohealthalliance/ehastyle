86a27bf9fef5121c945e922714328abe7f3060cb
Noam Ross <ross@ecohealthalliance.org>
2017-10-31 09:48:30 -0400

    Add ehaletter

---
+++ R/ehaletter.R
@@ -0,0 +1,11 @@
+#' Create a letter on EHA letterhead
+#'
+#' This is a thin wrapper around the [linl::linl()] function which prints letters,
+#' automatically using the EHA letterhead.  See that function's options for
+#' further
+#' @export
+#' @importFrom linl linl
+eha_letter <- function(...) {
+  letterhead <- system.file("EHA_letterhead.pdf", package = "ehastyle")
+  linl(..., pandoc_args=paste0("--metadata=letterhead:", letterhead))
+}
