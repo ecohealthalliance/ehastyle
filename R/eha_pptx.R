2d221337fdcbf5c6ffcb8b45fcc8496d94dc1eb3
Collin Schwantes <schwantes@ecohealthalliance.org>
2021-06-16 11:40:58 -0400

    added documentation for eha_pptx.R  and fixed issue with image size in ppt

--- R/eha_pptx.R
+++ R/eha_pptx.R
@@ -1,3 +1,27 @@
+#' EHA Powerpoint for Predict
+#'
+#' Styles for eha rmarkdown ppt. After install the markdown template will be
+#' available when you choose to create a new markdown file.
+#'
+#' @param aspect String. Aspect ratio for power point slides.
+#' Either "16x9" or "4x3". Default is "16x9"
+#' @param toc Logical. Should the slides have a table of contents?
+#' Default is FALSE
+#' @param toc_depth Numeric. How many levels should the table of contents have.
+#' Default is 2.
+#' @param fig_width Numeric. Width of figures. Default is 9.4
+#' @param fig_height Numeric. Height of figures. Default is 4.24
+#' @param fig_caption Logical. Should figures have captions? Default is TRUE
+#' @param df_print String. Print data frames? Default is "default"
+#' @param keep_md Logical. Should markdown document be kept? Default is FALSE
+#' @param cache_prefix String. Directory for cache contents. Default is "cache/"
+#' @param smart Logical. Produce typographically correct outs. Default is TRUE
+#' @param md_extensions String. Additional \href{https://pandoc.org/MANUAL.html#pandocs-markdown}{markdown extensions}.
+#' Default is NULL
+#' @param slide_level Numeric. Defines the heading level that defines individual
+#'  slides. Default is NULL
+#' @param pandoc_args Arguments to pass to pandoc. See \code{\link[MASS]{stats}}
+#'
 #' @importFrom rmarkdown knitr_options output_format pandoc_available powerpoint_presentation pandoc_options
 #' @importFrom officer read_pptx on_slide ph_with external_img
 #' @export
@@ -50,8 +74,14 @@ eha_pptx <- function(aspect = "16x9",
                                      alt = "eha-logo"
         )
 
-        pf <- ph_with(pf, eha_logo_169,
-                      left = 1.1, top = 0.89)
+        pf <- ph_with(pf,
+                      eha_logo_169,
+                      ph_location(
+                        width = 8.7,
+                        height = 2.24,
+                        left = 1.1,
+                        top = 0.89)
+                      )
       } else if (aspect == "4x3") {
 
         eha_logo_43 <- external_img(src = eha_logo_path,
@@ -61,8 +91,13 @@ eha_pptx <- function(aspect = "16x9",
         )
 
         pf <- ph_with(pf,
-                       eha_logo_43,
-                       left = 0.82, top = 1.22)
+                      eha_logo_43,
+                      ph_location(
+                        width = 6.52,
+                        height = 1.68,
+                        left = 0.82,
+                        top = 1.22)
+                      )
       }
     }
     print(pf, target = output_file)
